db73d4eb70e9c633635f029b893553d860a80e9a
User <flayrys100@gmail.com>
2024-02-22 21:10:30 -0300

    22/02/2024 todo

--- Portifólio/Todo/todo/src/List.js
+++ Portifólio/Todo/todo/src/List.js
@@ -1,9 +1,12 @@
 import React from "react";
 
 function List(props) {
+
     return(
         <ul>
-            {props.items.map(item => <li>{item}</li>)}
+            {props.items.map(item => <li key={item.id}>{item.text}
+            <button onClick={() => props.onItemDeleted(item)}><img  height="30"alt="img"src='./delete.png'></img></button>
+            </li>)}
         </ul>
     )
 };
--- Portifólio/Todo/todo/src/Todo.js
+++ Portifólio/Todo/todo/src/Todo.js
@@ -1,16 +1,25 @@
 import React, { useState } from "react";
 import List from './List';
+import Item from "./Item";
 import TodoForm from "./TodoForm";
 import './Todo.css';
 
 export default function Todo() {
     const [items, setItems] = useState([])
 
-    function onAddItem(item) {
+    function onAddItem(text) {
+        let item = new Item(text)
         setItems([...items, item])
         // aqui o setItems esta salvando o ITEM(PARAMETRO) dentro dos ITEM
         //e tambem esta criando um objeto por meio do OPERADOR SPREAD(...) e adicionando o
         // novo item ao final do objeto de ITEMS
+    }
+    function onItemDeleted(item) {
+        //essa funcão pega o item por meio de PARAMETRO (item)apagado e tira ele da lista basead no ID dele e retorna e 
+        // SALVA em items.
+        let filteredItems = items.filter(it=>it.id !== item.id)
+
+        setItems(filteredItems)
     }
 
     return(
@@ -18,7 +27,7 @@ export default function Todo() {
         <h1>Todo</h1>
 
         <TodoForm onAddItem={onAddItem}/>
-       <List items={items}/>
+       <List onItemDeleted={onItemDeleted} items={items}/>
 
 
         </div>
